cac215f51606e4a6761c7254705b1388b2d2aa87
Gerardo Gutierrez <gerardo.gutierrez@mobilityquotient.com>
2023-02-06 21:14:31 -0600

    ADDED: New hook

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,26 @@
+import { useNavigate, useLocation } from 'react-router-dom'
+import queryString from 'query-string';
 import { HeroCard } from '../components/HeroCard';
+import { useForm } from '../../hooks';
 
 export const SearchPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const query = queryString(location.search)
+
+  console.log(query)
+
+  const {searchText, onInputChange} = useForm({
+    searchText: ''
+  });
+
+  const onSearchSubmit = event => {
+    event.preventDefault();
+    if(searchText.trim().length <= 1) return;
+
+    navigate(`?q=${searchText.toLowerCase().trim()}`)
+  }
   return (
     <>
       <h1>Search</h1>
@@ -9,7 +29,7 @@ export const SearchPage = () => {
         <div className='col-5'>
           <h4>Searching</h4>
           <hr />
-          <form action=''>
+          <form onSubmit={onSearchSubmit}>
             <input
               type='text'
               name='searchText'
@@ -17,6 +37,8 @@ export const SearchPage = () => {
               placeholder='Search a hero'
               className='form-control'
               autoComplete='off'
+              value={searchText}
+              onChange={onInputChange}
             />
             <button type='submit' className='btn btn-outline-primary mt-3'>
               Search
